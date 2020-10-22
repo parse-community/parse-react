98ece708eac8124114d15747e59c2669d35ebce9
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2020-10-22 16:14:24 -0700

    Using @parse/react as a dependency for react-ts-todo

--- examples/react-ts-todo/src/App.tsx
+++ examples/react-ts-todo/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import parseReact from '@parse/react';
 
 function App() {
   return (
@@ -8,16 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {parseReact}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
