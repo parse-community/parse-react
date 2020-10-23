b1e9e9a729ffb33024b57d5a140b10e526cfd951
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2020-10-22 19:34:37 -0700

    Transform initial example into a hook

--- examples/react-ts-todo/src/App.tsx
+++ examples/react-ts-todo/src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import parseReact from '@parse/react';
+import { useParseQuery } from '@parse/react';
 
 function App() {
+  const objects = useParseQuery();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {parseReact}
-        </p>
+        <ul>
+          {objects.map(object => (
+            <li>{object}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
---
+++ packages/parse-react/src/useParseQuery.ts
@@ -0,0 +1,10 @@
+const useParseQuery = () => {
+  const objects = [
+    1,
+    2,
+    3
+  ];
+  return objects;
+};
+
+export default useParseQuery;
