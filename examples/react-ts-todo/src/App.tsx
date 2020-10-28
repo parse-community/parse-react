c7e2a2b6599f57cfb3e2ff76ff1f8f8779f9a852
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2020-10-27 22:32:18 -0700

    Included all states and actions on reducer, completed the hook lifecycle

--- examples/react-ts-todo/src/App.tsx
+++ examples/react-ts-todo/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Parse from 'parse';
 import { useParseQuery } from '@parse/react';
 import logo from './logo.svg';
@@ -8,32 +8,59 @@ Parse.serverURL = 'http://localhost:1337/parse';
 Parse.initialize('APPLICATION_ID', 'JAVASCRIPT_KEY');
 
 function App() {
+  const [
+    hideDone,
+    setHideDone
+  ] = useState(false);
+
+  const query = new Parse.Query('Todo');
+
+  if (hideDone) {
+    query.notEqualTo('done', true);
+  }
+
   const {
+    isLive,
     isLoading,
-    objects: todos,
-    error
-  } = useParseQuery(new Parse.Query('Todo'));
+    isSyncing,
+    objects,
+    error,
+    reload
+  } = useParseQuery(query);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {isLoading ? (
+        <button
+          onClick={() => setHideDone(!hideDone)}
+        >
+          {hideDone ? 'Unhide' : 'Hide'} done todos
+        </button>
+        {isLoading && (
           <p>Loading...</p>
-        ) : (
-          <>
-            {todos && (
-              <ul>
-                {todos.map(todo => (
-                  <li>{todo.get('title')}</li>
-                ))}
-              </ul>
-            )}
-            {error && (
-              <p>{error.message}</p>
-            )}
-          </>
         )}
+        {isLive && (
+          <p>Live!</p>
+        )}
+        {isSyncing && (
+          <p>Syncing...</p>
+        )}
+        {objects && (
+          <ul>
+            {objects.map(object => (
+              <li>{object.get('title')}</li>
+            ))}
+          </ul>
+        )}
+        {error && (
+          <p>{error.message}</p>
+        )}
+        <button
+          onClick={reload}
+        >
+          Reload
+        </button>
       </header>
     </div>
   );
--- packages/parse-react/src/useParseQuery.ts
+++ packages/parse-react/src/useParseQuery.ts
@@ -1,71 +1,302 @@
-import { useReducer, useEffect } from 'react';
+import { useReducer, useCallback, useEffect } from 'react';
 import Parse from 'parse';
 
-export interface ParseQueryState<T extends Parse.Object<Parse.Attributes>> {
+interface ResultState<T extends Parse.Object<Parse.Attributes>> {
   isLoading: boolean;
+  isLive: boolean;
+  isSyncing: boolean;
   objects?: T[];
   error?: Error;
 }
 
-enum ActionTypes {
-  Confirm,
-  Fail
+interface State<T extends Parse.Object<Parse.Attributes>> extends ResultState<T> {
+  queryId: number;
 }
 
-const useParseQuery = <T extends Parse.Object<Parse.Attributes>>(query: Parse.Query<T>): ParseQueryState<T> => {
-  const confirm = (objects: T[]) => ({
-    type: ActionTypes.Confirm,
-    payload: {
-      objects
-    }
-  } as const);
+enum ActionTypes {
+  LoadLocalDatastoreObjects,
+  SetIsLive,
+  SetIsSyncing,
+  LoadParseServerObjects,
+  Fail,
+  Reset
+}
 
-  const fail = (error: Error) => ({
-    type: ActionTypes.Fail,
-    payload: {
-      error
-    }
-  } as const);
+type LoadLocalDatastoreObjectsAction<T extends Parse.Object<Parse.Attributes>> = {
+  type: ActionTypes.LoadLocalDatastoreObjects,
+  payload: {
+    queryId: number,
+    objects: T[]
+  }
+};
 
-  const [state, dispatch] = useReducer(
-    (state: ParseQueryState<T>, action: ReturnType<typeof confirm | typeof fail>) => {
-      switch (action.type) {
-        case ActionTypes.Confirm: {
-          return {
-            isLoading: false,
-            objects: action.payload.objects
-          };
-        }
-        case ActionTypes.Fail: {
-          return {
-            isLoading: false,
-            error: action.payload.error
-          };
-        }
-      }
-      return state;
-    },
+// const loadLocalDatastoreObjects = <T extends Parse.Object<Parse.Attributes>>(
+//   queryId: number,
+//   objects: T[]
+// ): LoadLocalDatastoreObjectsAction<T> => ({
+//   type: ActionTypes.LoadLocalDatastoreObjects,
+//   payload: {
+//     queryId,
+//     objects
+//   }
+// });
+
+const setIsLive = (queryId: number, isLive: boolean) => ({
+  type: ActionTypes.SetIsLive,
+  payload: {
+    queryId,
+    isLive
+  }
+} as const);
+
+const setIsSyncing = (queryId: number, isSyncing: boolean) => ({
+  type: ActionTypes.SetIsSyncing,
+  payload: {
+    queryId,
+    isSyncing
+  }
+} as const);
+
+type LoadParseServerObjectsAction<T extends Parse.Object<Parse.Attributes>> = {
+  type: ActionTypes.LoadParseServerObjects,
+  payload: {
+    queryId: number,
+    objects: T[]
+  }
+};
+
+const loadParseServerObjects = <T extends Parse.Object<Parse.Attributes>>(
+  queryId: number,
+  objects: T[]
+): LoadParseServerObjectsAction<T> => ({
+  type: ActionTypes.LoadParseServerObjects,
+  payload: {
+    queryId,
+    objects
+  }
+});
+
+const fail = (queryId: number, error: Error) => ({
+  type: ActionTypes.Fail,
+  payload: {
+    queryId,
+    error
+  }
+} as const);
+
+const reset = () => ({
+  type: ActionTypes.Reset
+} as const);
+
+type Action<T extends Parse.Object<Parse.Attributes>> =
+  LoadLocalDatastoreObjectsAction<T> |
+  LoadParseServerObjectsAction<T> |
+  ReturnType<
+    typeof setIsLive |
+    typeof setIsSyncing |
+    typeof fail |
+    typeof reset
+  >;
+
+type Reducer<T extends Parse.Object<Parse.Attributes>> = (
+  state: State<T>,
+  action: Action<T>
+) => State<T>;
+
+const initialState = {
+  queryId: 1,
+  isLoading: true,
+  isLive: false,
+  isSyncing: false
+};
+
+const reducer = <T extends Parse.Object<Parse.Attributes>>(
+  state: State<T>,
+  action: Action<T>
+): State<T> => {
+  if (action.type === ActionTypes.Reset) {
+    return {
+      ...initialState,
+      queryId: state.queryId + 1
+    };
+  } else if (
+    action.payload.queryId !== state.queryId ||
+    state.error
+  ) {
+    return state;
+  }
+
+  switch (action.type) {
+    case ActionTypes.LoadLocalDatastoreObjects: {
+      return {
+        ...state,
+        isLoading: false,
+        objects: state.isLoading ? action.payload.objects : state.objects
+      };
+    }
+
+    case ActionTypes.SetIsLive: {
+      return {
+        ...state,
+        isLive: action.payload.isLive
+      };
+    }
+
+    case ActionTypes.SetIsSyncing: {
+      return {
+        ...state,
+        isSyncing: action.payload.isSyncing
+      };
+    }
+
+    case ActionTypes.LoadParseServerObjects: {
+      return {
+        ...state,
+        isLoading: false,
+        isSyncing: false,
+        objects: action.payload.objects
+      };
+    }
+
+    case ActionTypes.Fail: {
+      return {
+        ...state,
+        isLoading: false,
+        isLive: false,
+        isSyncing: false,
+        error: action.payload.error
+      };
+    }
+  }
+};
+
+export interface UseParseQueryOptions {
+  enableLocalDatastore?: boolean;
+  enableLiveQuery?: boolean;
+}
+
+export interface UseParseQueryResult<T extends Parse.Object<Parse.Attributes>> extends ResultState<T> {
+  reload: () => void;
+}
+
+const useParseQuery = <T extends Parse.Object<Parse.Attributes>>(
+  query: Parse.Query<T>,
+  options?: UseParseQueryOptions
+): UseParseQueryResult<T> => {
+  const queryString = JSON.stringify(query.toJSON());
+
+  const {
+    enableLocalDatastore = true,
+    enableLiveQuery = true
+  } = options || {};
+
+  const [
     {
-      isLoading: false
-    }
+      queryId,
+      isLoading,
+      isLive,
+      isSyncing,
+      objects,
+      error
+    },
+    dispatch
+  ] = useReducer(reducer as Reducer<T>, initialState);
+
+  const findFromParseServer = useCallback(
+    async () => {
+      if (enableLocalDatastore || enableLiveQuery) {
+        dispatch(setIsSyncing(queryId, true));
+      }
+
+      try {
+        dispatch(loadParseServerObjects(
+          queryId,
+          await query.find()
+        ));
+      } catch (e) {
+        dispatch(fail(queryId, e));
+      }
+    },
+    [query, enableLocalDatastore, enableLiveQuery, queryId]
   );
 
-  const load = async () => {
-    try {
-      dispatch(confirm(await query.find()));
-    } catch (e) {
-      dispatch(fail(e));
-    }
-  };
+  const subscribeLiveQuery = useCallback(
+    (): (() => void) => {
+      let liveQuerySubscription: Parse.LiveQuerySubscription | undefined;
+
+      const subscribe = async () => {
+        try {
+          liveQuerySubscription = await query.subscribe();
+        } catch (e) {
+          dispatch(fail(queryId, e));
+
+          return;
+        }
+    
+        liveQuerySubscription.on('open', () => {
+          dispatch(setIsLive(queryId, true));
+
+          findFromParseServer();
+        });
+    
+        liveQuerySubscription.on('close', () => {
+          dispatch(setIsLive(queryId, false));
+        });
+      };
+
+      const subscribePromise = subscribe();
+    
+      const unsubscribe = async () => {
+        await subscribePromise;
+
+        if (liveQuerySubscription) {
+          liveQuerySubscription.unsubscribe();
+        }
+
+        dispatch(setIsLive(queryId, false));
+      }
+
+      return unsubscribe;
+    },
+    [query, queryId, findFromParseServer]
+  );
 
   useEffect(
     () => {
-      load();
+      let unsubscribeLiveQuery: (() => void) | undefined;
+
+      if (enableLiveQuery) {
+        unsubscribeLiveQuery = subscribeLiveQuery();
+      } else {
+        findFromParseServer();
+      }
+
+      return unsubscribeLiveQuery;
     },
-    []
+    [queryId]
   );
 
-  return state;
+  const reload = () => {
+    dispatch(reset());
+  };
+
+  useEffect(
+    () => reload,
+    [
+      queryString,
+      enableLocalDatastore,
+      enableLiveQuery
+    ]
+  );
+
+  return {
+    isLoading,
+    isLive,
+    isSyncing,
+    objects,
+    error,
+    reload
+  };
 };
 
 export default useParseQuery;
