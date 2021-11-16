0d41b00c14d29b6e6d8802fb55e6045109e79c8a
Mohamed Gamal <Mohamed.abugalala@gmail.com>
2021-11-16 16:46:22 +0200

    Fix enableLocalDataStore must be called after initialize

--- packages/parse-react/src/index.ts
+++ packages/parse-react/src/index.ts
@@ -5,7 +5,7 @@ global.Parse = Parse;
 export * from '@parse/react-base';
 
 export const initializeParse = (serverURL: string, applicationId: string, javascriptKey: string) => {
-  Parse.enableLocalDatastore();
   Parse.serverURL = serverURL;
   Parse.initialize(applicationId, javascriptKey);
+  Parse.enableLocalDatastore();
 };
